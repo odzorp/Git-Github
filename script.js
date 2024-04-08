b8395335d4bf5b32637ce9aeeeb0de1e5b67d74b
manWonders <odzorp3@gmail.com>
2024-04-08 08:31:48 +0000

    My First Release of gitHub repo

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('clickMeButton');
+
+    button.addEventListener('click', function() {
+        alert('You clicked the button!');
+    });
+});
